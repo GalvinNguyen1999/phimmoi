d8492ce2c1fd79e838f8f50e17405f61d07fb0ca
TABLET-T9OG6L0B\Galvin <galvinnguyen1999@gmail.com>
2023-10-03 00:09:00 +0700

    Carousel UI

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,12 @@
+import Carousel from '@/modules/Home/Carousel'
+
 export default function Home() {
-  return <div>Hello World</div>
+  return (
+    <main className='flex w-full h-full p-4'>
+      <div className='w-full lg:w-8/12 relative'>
+        <Carousel />
+      </div>
+      <div className='hidden lg:block lg:w-4/12'>Box 2</div>
+    </main>
+  )
 }
--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -6,10 +6,10 @@ import Logo from './Logo'
 
 const Header = () => {
   return (
-    <header className='flex items-center justify-between px-3 h-[42px] shadow-md relative md:h-14 lg:h-20'>
+    <header className='flex items-center justify-between px-3 h-[42px] shadow-md relative md:h-14 lg:h-20 w-full'>
       <Logo />
-      <Navbar className='hidden lg:block lg:w-3/6' />
-      <div className='w-3/5 lg:flex lg:gap-2 lg:w-auto'>
+      <Navbar className='hidden lg:block lg:w-4/6' />
+      <div className='md:w-3/6 w-2/6 lg:flex lg:gap-2'>
         <SearchHeader className='border-2 rounded-lg overflow-hidden shadow-md w-full justify-between hidden md:flex lg:flex-1' />
         <div className='hidden lg:flex gap-2 items-center lg:w-2/6'>
           <Button className='border-green-500 hover:bg-green-500'>Search</Button>
