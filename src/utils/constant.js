5a143e12e18ed1c5ad84d0c2c9d35f2429f0ad79
TABLET-T9OG6L0B\Galvin <galvinnguyen1999@gmail.com>
2023-10-05 23:15:30 +0700

    Now Playing

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,20 +1,51 @@
 export const navigations = [
   {
     title: 'Đang chiếu',
-    href: '/',
+    href: '/now-playing',
   },
   {
     title: 'Phim sắp chiếu',
-    href: '/',
+    href: '/up-coming',
   },
   {
     title: 'Phim phổ biến',
-    href: '/',
+    href: '/popular',
+  }
+]
+
+export const listYears = [
+  {
+    id: 0,
+    year: 2016
   },
   {
-    title: 'year',
-    href: '/',
+    id: 1,
+    year: 2017
   },
+  {
+    id: 2,
+    year: 2018
+  },
+  {
+    id: 3,
+    year: 2019
+  },
+  {
+    id: 4,
+    year: 2020
+  },
+  {
+    id: 5,
+    year: 2021
+  },
+  {
+    id: 6,
+    year: 2022
+  },
+  {
+    id: 7,
+    year: 2023
+  }
 ]
 
 export const carousels = [
